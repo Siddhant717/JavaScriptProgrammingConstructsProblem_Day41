bec1c13a43100dc565dca6acbd2407f730490d7a
Siddhant <santoshshrivastava39@gmail.com>
2022-07-31 00:49:36 +0530

    Find second Largest and second Smallest Random Number

--- Array_And_Map_Problems.js
+++ Array_And_Map_Problems.js
@@ -1,45 +1,50 @@
+
+//creating function to generate three digit random number
 function GenerateThreeDigitRandomNumber() {
     return Math.floor(Math.random() * 900 + 100);
-  }
-//Array of 3 Digit Number
+}
+
+
 let array = new Array();
-//Storing array elements
-for (let i = 0; i < 10; i++){
+//storing the random number into array
+for (let i = 0; i < 10; i++) {
     array.push(GenerateThreeDigitRandomNumber());
 }
+//printing the elements in array
 console.log("Ten 3 digits Random numbers are: " + array);
 
-
-function FindLargestAndSmallest(arrayOfElements){
+//creating function to find 2nd largest and 2nd smallest elements
+function FindLargestAndSmallest(arrayOfElements) {
     let firstLargest = 0,
         secondlargest = 0;
     let firstSmallest = 1000,
         secondSmallest = 1000;
     for (let i = 0; i < arrayOfElements.length; i++) {
         if (arrayOfElements[i] > firstLargest) {
-        secondlargest = firstLargest;
-        firstLargest = arrayOfElements[i];
+            secondlargest = firstLargest;
+            firstLargest = arrayOfElements[i];
         } else if (
-        arrayOfElements[i] > secondlargest &&
-        arrayOfElements[i] != firstLargest
+            arrayOfElements[i] > secondlargest &&
+            arrayOfElements[i] != firstLargest
         )
-        secondlargest = arrayOfElements[i];
+            secondlargest = arrayOfElements[i];
     }
     for (let i = 0; i < arrayOfElements.length; i++) {
         if (arrayOfElements[i] < firstSmallest) {
-        secondSmallest = firstSmallest;
-        firstSmallest = arrayOfElements[i];
+            secondSmallest = firstSmallest;
+            firstSmallest = arrayOfElements[i];
         } else if (
-        arrayOfElements[i] < secondSmallest &&
-        arrayOfElements[i] != firstSmallest
+            arrayOfElements[i] < secondSmallest &&
+            arrayOfElements[i] != firstSmallest
         )
-        secondSmallest = arrayOfElements[i];
+            secondSmallest = arrayOfElements[i];
     }
-console.log("First largest number is: " + firstLargest);
-console.log("Second largest number is: " + secondlargest);
-console.log("First smallest number is: " + secondSmallest);
-console.log("Second smallest number is: " + firstSmallest);
+    //printing the results
+    console.log("First largest number is: " + firstLargest);
+    console.log("Second largest number is: " + secondlargest);
+    console.log("First smallest number is: " + secondSmallest);
+    console.log("Second smallest number is: " + firstSmallest);
 
 };
-
+//calling the function
 FindLargestAndSmallest(array);
